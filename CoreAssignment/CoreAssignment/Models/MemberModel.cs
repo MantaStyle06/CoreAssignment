d661fb149af7bf64070bec034b5ebec61d745221
ronel <ronel.ivp@gmail.com>
2018-04-24 15:56:54 +0800

    #study appsettings

---
+++ CoreAssignment/CoreAssignment/Models/MemberModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAssignment.Models
+{
+    public class MemberModel
+    {
+        public string member_id { get; set; }
+
+        public string email { get; set; }
+
+        public string c_email { get; set; }
+
+        public string password { get; set; }
+
+        public string c_password { get; set; }
+
+        public string g_name { get; set; }
+
+        public string f_name { get; set; }
+
+        public string g_namek { get; set; }
+
+        public string f_namek { get; set; }
+
+        public string zip_code { get; set; }
+
+        public string pref { get; set; }
+
+        public string address { get; set; } 
+
+        public string tel { get; set; }
+
+        public string sex { get; set; }
+
+
+    }
+}
